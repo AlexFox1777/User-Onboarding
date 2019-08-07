6ae8b473917544ae4e07d74173422c85f19614c4
Sasha <mix.filipov@mail.ru>
2019-08-07 13:56:44 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Form from "./components/form/Form";
+
+
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+        };
+    }
+
+
+    render() {
+        return (
+            <div>
+                <Form />
+            </div>
+        );
+    }
+}
+
+export default App;
